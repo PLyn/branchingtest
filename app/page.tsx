37e8178cddfaaeb8fec4b2a1d58dd12b8d31c445
PLyn <plyn8750@gmail.com>
2024-09-20 12:11:22 -0500

    new change

--- app/page.tsx
+++ app/page.tsx
@@ -21,12 +21,7 @@ export default async function Index() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
-        </div>
-      </nav>
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16"></nav>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
