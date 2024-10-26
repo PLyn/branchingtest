2dc9e870ded4747534d0bb81e3b691090999bb53
PLyn <plyn8750@gmail.com>
2024-10-26 13:27:58 -0500

    asdqw

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,7 +1,13 @@
 insert into
+public.employees (name)
+values
+('Erlich Bachman'),
+('Richard Hendricks'),
+('Monica Hall');
+
+insert into
 public.test (name)
 values
 ('asd dqwde'),
 ('asdffgew'),
 ('Msdfghtll');
-
